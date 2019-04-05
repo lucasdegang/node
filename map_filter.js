72dd59ebd23accbcc74a2df8e9c99cccdb506e63
d3g4ng <lucas.degang98@gmail.com>
2019-04-04 22:41:13 -0300

    ajustes e testes em relação aos conceitos de .map e .filter

--- map_filter.js
+++ map_filter.js
@@ -1,27 +1,37 @@
 let carros = [
-    '{"marca": "VW", "modelo": "golf", "valor": 89990, "ano": 2018}',
-    '{"marca": "Fiat", "modelo": "Uno", "valor": 32500, "ano": 2017}',
+    '{"marca": "VW", "modelo": "Golf", "valor": 89990, "ano": 2017}',
+    '{"marca": "Fiat", "modelo": "Uno", "valor": 32500, "ano": 2015}',
     '{"marca": "Mercedes", "modelo": "C180", "valor": 55990, "ano": 2014}'
 ]
 
 let jsonParaObjeto = json => JSON.parse(json)
 
-let mapFiltroAnoValor = json => json.ano >= 2017 && json.valor > 30000 //? `${json.modelo}` : null
+// ================ MAP ================ filtros .map()
+// Obs.:
+// Informar qual deve ser o valor de retorno da arrow function pelo comando ? true : false
+// Montar a condicional em uma unica arrow function com o conectivo E, conjuncao
 
-let filterAno = json => json.ano >= 2015 
-let filterValor = json => json.valor >= 30000
+let mapAnoValor = json => json.ano <= 2016 && json.valor < 100000 ? `${json.ano}` : false
+let mapAno = json => json.ano <= 2016 ? `${json.ano}` : false
+let mapValor= json => json.valor < 100000 ? `${json.valor}` : false
 
-let filterSort = json => json.modelo
+console.log("map 1 - Sem .sort() " + carros.map(jsonParaObjeto).map(mapAnoValor)) //SEM ORDENACAO
+console.log("map 2 - Com .sort() " + carros.map(jsonParaObjeto).map(mapAnoValor).sort()) //ORDENA DE ACORDO COM O RESULTADO DE .map(mapAnoValor)
+/*ERRO*/ console.log("map 3 - sem .sort() " + carros.map(jsonParaObjeto).map(mapAno).map(mapValor))
+/*ERRO*/ console.log("map 4 - com .sort() " + carros.map(jsonParaObjeto).map(mapAno).map(mapValor).sort())
 
+// ================ Filter ================ filtros .filter()
+// Obs.: 
+// .sort() nao funciona no .filter()
+// Nao funciona console.log("mensagem" + comando.filter() , pois retorna [object object])
 
+let filterAnoValor = json => json.ano <= 2016 && json.valor < 100000
+let filterMarca = json => json.marca
+let filterModelo = json => json.modelo 
+let filterAno = json => json.ano <= 2016
+let filterValor = json => json.valor < 100000
 
-// map = carros[original] > carros[copia]
-console.log(carros.map(jsonParaObjeto).map(mapFiltroAnoValor))
-
-// filter
-console.log(carros.map(jsonParaObjeto).filter(filterAno).filter(filterValor))
-
-// sort
-console.log(
-    carros.map(jsonParaObjeto).map(filterSort).sort()
-)
+console.log(carros.map(jsonParaObjeto).filter(filterAnoValor) )
+console.log(carros.map(jsonParaObjeto).filter(filterAnoValor).sort() )
+console.log(carros.map(jsonParaObjeto).filter(filterAno).filter(filterValor) )
+console.log(carros.map(jsonParaObjeto).filter(filterAno).filter(filterValor).sort() )
